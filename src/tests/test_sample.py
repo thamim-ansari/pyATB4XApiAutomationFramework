be0344e6703daea2af910844d29d08b85b6f9b50
thamim-ansari <thamimansari0117@gmail.com>
2025-03-24 16:21:39 +0530

    Added 1 more file

---
+++ src/tests/test_sample.py
@@ -0,0 +1,6 @@
+import pytest
+import allure
+
+@allure.title("Sample Testcase")
+def test_sample():
+    assert True == True
---
+++ src/tests/test_sample2.py
@@ -0,0 +1,6 @@
+import pytest
+import allure
+
+@allure.title("Sample Testcase 2")
+def test_sample():
+    assert 1+1 == 2
